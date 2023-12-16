2f4029351bbdea60c09f7814864dfa75fdf77ad1
Sai Rohit <sairohitp007@gmail.com>
2023-12-16 15:21:13 +0530

    react icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from "react";
 import FooterCSS from "./Footer.module.css";
+
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+
 
 const Footer = () => {
   return (
     <div id={FooterCSS.supernav}>
-      
       <div className="subbody">
-
         <nav className={FooterCSS.flex}>
-          
-        <span class="material-symbols-outlined">
-article
-</span>
-        <span class="material-symbols-outlined">
-mail
-</span>
-
+          <span class="material-symbols-outlined">article</span>
+          <span class="material-symbols-outlined">mail</span>
+          <span class="symbols"><FaLinkedinIn /></span>
+          <span class="symbols"><FaGithub /></span>
         </nav>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
