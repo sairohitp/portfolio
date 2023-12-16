581908df12a723ea965437d0dbeb8906c8c85eac
Sai Rohit <sairohitp007@gmail.com>
2023-12-16 15:05:59 +0530

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,44 +1,20 @@
 import React from 'react'
+import FooterCSS from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div id={NavbarCSS.supernav}>
+    <div id={FooterCSS.supernav}>
       
       <div className="subbody">
 
-        <nav className={NavbarCSS.flex}>
-          <div>
-            <p>
-              <b>Hello, I’m Sai Rohit</b>
-            </p>
-          </div>
-
-          <div className={NavbarCSS.flex}>
-            <a href="">
-              <p>Experience</p>
-            </a>
-            <a href="">
-              <p>Projects</p>
-            </a>
-            <a href="">
-              <p>Education</p>
-            </a>
-            <a href="">
-              <p>Achievements</p>
-            </a>
-          </div>
-
-          <div>
-
-            <div>
-              <span class="material-symbols-outlined navIcon">language</span>
-            </div>
-
-            <div>
-              <span class="material-symbols-outlined navIcon">contrast</span>
-            </div>
-
-          </div>
+        <nav className={FooterCSS.flex}>
+          
+        <span class="material-symbols-outlined">
+article
+</span>
+        <span class="material-symbols-outlined">
+mail
+</span>
 
         </nav>
         
