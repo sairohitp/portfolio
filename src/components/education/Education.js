7079331eabbeaf094f20efdcfe6195f1f027ddf6
Sai Rohit <sairohitp007@gmail.com>
2024-01-14 23:53:00 +0530

    education template page

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -3,38 +3,38 @@ import EducationCSS from "./Education.module.css"
 
 const Education = (props) => {
     return (
-        
-<>
-                    <div className={EducationCSS.innerstrip}>
-                        <div>
-                            <img src={props.educationIMG} alt="srm" id={EducationCSS.img}></img>
-                        </div>
-                        <div className={EducationCSS.innerstriptext}>
-                            <div>
-                                <h3>{props.educationInstituteTitle}</h3>
-                                <h6>
-                                    {props.educationMajor}
-                                </h6>
-                                <h6>
-                                    {props.educationInstituteLocation}
-                                </h6>
-                            </div>
 
-                            <div>
-                                <h6>Activities & Records:</h6>
-                                {props.activities}
-                            </div>
-                            
-                            <div>
-                                <h6><b>Skills:</b> {props.educationSkillsList}</h6>
-                            </div>
-                        </div>
-                    </div>
+        <>
+            <div className={EducationCSS.innerstrip}>
+                <div>
+                    <img src={props.educationIMG} alt="srm" id={EducationCSS.img}></img>
+                </div>
+                <div className={EducationCSS.innerstriptext}>
                     <div>
-                        <h6>{props.educationDuration}</h6>
+                        <h3>{props.educationInstituteTitle}</h3>
+                        <h6>
+                            {props.educationMajor}
+                        </h6>
+                        <h6>
+                            {props.educationInstituteLocation}
+                        </h6>
                     </div>
 
-</>
+                    <div>
+                        <h6>Activities & Records:</h6>
+                        {props.activities}
+                    </div>
+
+                    <div>
+                        <h6><b>Skills:</b> {props.educationSkillsList}</h6>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h6>{props.educationDuration}</h6>
+            </div>
+
+        </>
     );
 };
 
